3dea1867eaf33091d33beed2dff91e45821ffbd1
lucyoz <lucyoz.kwon@gmail.com>
2023-04-17 18:14:21 +0900

    readme: 0417

---
+++ src/etc/baekjoon/BaekJoon1253.java
@@ -0,0 +1,18 @@
+package etc.baekjoon;
+
+import java.util.Scanner;
+
+public class BaekJoon1253 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+
+    long[] A = new long[N];
+
+    for (int i = 0; i < N; i++) {
+      A[i] = sc.nextInt();
+    }
+  }
+
+}
