ddd0b69f0458ea3e5dd1640aa1b767c7e9d4e688
lucyoz <lucyoz.kwon@gmail.com>
2023-05-09 10:30:53 +0900

    프로그래머스: 피로도 (DFS, 재귀 이용)

---
+++ src/etc/Fatigue.java
@@ -0,0 +1,44 @@
+package etc;
+
+public class Fatigue {
+  int max = Integer.MIN_VALUE;
+
+  public void dfs(int k, int[][] dungeons, boolean[] visited){
+    int count = 0;
+    for(int i=0;i<visited.length;i++){
+      if(visited[i]){
+        count++;
+      }
+    }
+    if(count > max){
+      max = count;
+    }
+
+    for (int i = 0; i < dungeons.length;i++) {
+      if(!visited[i]){
+        if(dungeons[i][0]<=k){
+          visited[i] = true;
+          k -= dungeons[i][1];
+          dfs(k, dungeons, visited);
+          k+=dungeons[i][1];
+          visited[i] = false;
+        }
+      }
+    }
+
+  }
+
+  public int solution(int k, int[][] dungeons){
+    boolean[] visited = new boolean[dungeons.length];
+    dfs(k, dungeons, visited);
+
+    return max;
+  }
+
+  public static void main(String[] args) {
+    Fatigue fatigue = new Fatigue();
+    int[][] dungeons= {{80,20},{50,40},{30,10}};
+    System.out.println(fatigue.solution(80, dungeons));
+  }
+
+}
