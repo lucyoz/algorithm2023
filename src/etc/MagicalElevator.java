4d5f17618ac11e2297f168af41bd2da9fd7d985b
lucyoz <lucyoz.kwon@gmail.com>
2023-05-15 12:21:35 +0900

    프로그래머스: 마법의 엘리베이터 (통과)

--- src/etc/MagicalElevator.java
+++ src/etc/MagicalElevator.java
@@ -1,71 +1,27 @@
 package etc;
 
 public class MagicalElevator {
-  public long onesPlace(long num, long digit){
-    if(digit==1){
-      return num;
-    }
-    num = num % (int)(Math.pow(10, digit-1));
+  int[] cnt = {0,1,2,3,4,5,4,3,2,1};
+  int answer = Integer.MAX_VALUE;
 
-    if(num>5){
-      return 10-num;
-    } else {
-      return -num;
-    }
-  }
-
-  public int sumOfDigit(long num){
-    int result = 0;
-    String[] arr = String.valueOf(num).split("");
-
-    for(String a: arr){
-      result += Integer.parseInt(a);
-    }
-    return result;
-  }
-
-  public int solution(int storey){
-    int answer = 0;
-
-    long length = (int)(Math.log10(storey)+1);
-
-    long num = storey % (long)(Math.pow(10,length-1));
-    System.out.println(length);
-    System.out.println(num);
-
-    if (length==1){
-      if(storey > 5){
-        return 10-storey;
-      } else {
-        return storey;
-      }
-    } else if (length==2){
-      long one = storey % 10;
-      if(one > 5){
-        answer += (10-one);
-        storey += (10-one);
-      } else {
-        answer += one;
-        storey -= one;
-      }
-      answer += storey / 10;
-      return answer;
-    }
-    long one = onesPlace(num, length-2);
-    num += one;
-    storey += one;
-    if(one > 0){
-      answer += one;
-    } else {
-      answer -= one;
-    }
-
-    long newNum = (num < (Math.pow(10, length-1)-num)) ? num: (long)(Math.pow(10, length-1)-num);
-
-    answer += sumOfDigit(newNum);
-    answer += (storey + newNum) / (long)(Math.pow(10, length-1));
-
+  public int solution(int current){
+    helper(current, 0);
     return answer;
+  }
+
+  public void helper(int current, int value){
+    if(current < 10){
+      int p = current > 5 ? 1 : 0;
+      answer = Math.min(cnt[current]+p+value, answer);
+      return;
+    }
+
+    if(current % 10 <= 5){
+      helper(current / 10, cnt[current % 10] + value);
+    }
+    if(current % 10 >= 5){
+      helper(current / 10 + 1, cnt[current % 10] + value);
+    }
   }
 
   public static void main(String[] args) {
