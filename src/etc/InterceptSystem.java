0e0f71ffd1e5550c7fb1b73b36df330fe3b79ac1
lucyoz <lucyoz.kwon@gmail.com>
2023-05-18 10:28:37 +0900

    프로그래머스: 요격시스템

---
+++ src/etc/InterceptSystem.java
@@ -0,0 +1,37 @@
+package etc;
+
+import java.util.Arrays;
+
+public class InterceptSystem {
+
+  public int solution(int[][] targets){
+
+    //2번째 요소(end)를 기준으로 오름차순 정렬렬
+   Arrays.sort(targets, (o1, o2) -> {
+      if(o1[1]==o2[1]){
+        return o1[0]-o2[0];
+      }
+      return o1[1]-o2[1];
+    });
+
+   int cut = -1;
+   int answer = 0;
+
+    for (int i = 0; i < targets.length; i++) {
+      int left = targets[i][0];
+      int right = targets[i][1];
+      if(left >= cut){
+        answer++;
+        cut = right;
+      }
+    }
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    InterceptSystem system = new InterceptSystem();
+    int[][] targets = {{4,5},{4,8},{10,14},{11,13},{5,12},{3,7},{1,4}};
+    System.out.println(system.solution(targets));
+  }
+
+}
