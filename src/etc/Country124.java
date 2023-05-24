310194b8ea81087b47bb2b63bc99dac653c13802
lucyoz <lucyoz.kwon@gmail.com>
2023-05-24 10:48:18 +0900

    프로그래머스: 124 나라의 숫자

---
+++ src/etc/Country124.java
@@ -0,0 +1,37 @@
+package etc;
+
+public class Country124 {
+
+  public String solution(int n){
+    String answer = "";
+    String[] numbers = {"4","1","2"};
+
+    int num = n;
+    while(num > 0){
+      int remainder = num % 3;    //numbers의 인덱스가 됨 -> 0이면 4가
+      num /= 3;
+
+      if(remainder == 0){
+        num--;
+      }
+      answer = numbers[remainder] + answer;
+    }
+
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    Country124 country124 = new Country124();
+
+    System.out.println(country124.solution(1));
+    System.out.println(country124.solution(2));
+    System.out.println(country124.solution(3));
+    System.out.println(country124.solution(4));
+    System.out.println(country124.solution(5));
+    System.out.println(country124.solution(6));
+    System.out.println(country124.solution(7));
+    System.out.println(country124.solution(9));
+    System.out.println(country124.solution(10));
+  }
+
+}
