dc4f17c621d19aa05384f5fe22692ae8d89db266
lucyoz <lucyoz.kwon@gmail.com>
2023-05-24 11:16:28 +0900

    프로그래머스: 줄 서는 방법 (ArrayList-효율성)

---
+++ src/etc/LineUp.java
@@ -0,0 +1,39 @@
+package etc;
+
+import java.util.ArrayList;
+
+public class LineUp {
+
+  public int[] solution(int n, long k){
+    int[] answer = new int[n];
+
+    ArrayList<Integer> list = new ArrayList<>();
+    long factorial = 1;
+    int idx = 0;
+
+    for (int i = 1; i <= n; i++) {
+      factorial *= i;
+      list.add(i);
+    }
+
+    k--;
+
+    while(n>0){
+      factorial /= n;
+      int val = (int) (k/factorial);
+      answer[idx] = list.get(val);
+      list.remove(val);
+
+      k %= factorial;
+      idx++;
+      n--;
+    }
+
+    return answer;
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
