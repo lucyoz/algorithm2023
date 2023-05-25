d72d1dac692355be7c989ffb95d6eee0fdc2958b
lucyoz <lucyoz.kwon@gmail.com>
2023-05-25 12:25:21 +0900

    프로그래머스: KAKAO [1차]뉴스 클러스터링 (모든 조건 만족, 통과)

--- src/etc/kakao/NewsClustering.java
+++ src/etc/kakao/NewsClustering.java
@@ -11,8 +11,8 @@ public class NewsClustering {
       return 65536;
     }
 
-    str1 = str1.toLowerCase();
-    str2 = str2.toLowerCase();
+    str1 = str1.toUpperCase();
+    str2 = str2.toUpperCase();
 
     String[] str1Arr = new String[str1.length()-1];
     String[] str2Arr = new String[str2.length()-1];
@@ -20,14 +20,19 @@ public class NewsClustering {
     ArrayList<String> str1List = new ArrayList<>();
     ArrayList<String> str2List = new ArrayList<>();
 
+    String tmp = "";
     for (int i = 0; i < str1.length()-1; i++) {
-      str1Arr[i] = str1.substring(i, i+2);
-      str1List.add(str1.substring(i, i+2));
+      tmp = str1.substring(i, i+2);
+      if(tmp.matches("^[A-Z]+$")){
+        str1List.add(tmp);
+      }
     }
 
     for (int i = 0; i < str2.length()-1; i++) {
-      str2Arr[i] = str2.substring(i, i+2);
-      str2List.add(str2.substring(i, i+2));
+      tmp = str2.substring(i, i+2);
+      if(tmp.matches("^[A-Z]+$")){
+        str2List.add(tmp);
+      }
     }
 
     int unionSize = getUnion(str1List, str2List).size();
@@ -35,6 +40,11 @@ public class NewsClustering {
 
     System.out.println(unionSize);
     System.out.println(interSize);
+    if(interSize==0 && unionSize==0){
+      return 65536;
+    } else if(interSize==0 && unionSize>0){
+      return 0;
+    }
     answer = (int) ((double) interSize / (double) unionSize * (double) 65536);
 
     return answer;
@@ -74,9 +84,9 @@ public class NewsClustering {
 
     NewsClustering clustering = new NewsClustering();
 
-    System.out.println(clustering.solution("FRANCE","french"));
-    System.out.println(clustering.solution("handshake","shake hands"));
-    System.out.println(clustering.solution("aa1+aa2","AAAA12"));
+    //System.out.println(clustering.solution("FRANCE","french"));
+    //System.out.println(clustering.solution("handshake","shake hands"));
+    //System.out.println(clustering.solution("aa1+aa2","AAAA12"));
     System.out.println(clustering.solution("E=M*C^2","e=m*c^2"));
   }
 
