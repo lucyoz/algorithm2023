8e516d66e57bb24dbe2ac3adcde29879d40fa97f
lucyoz <lucyoz.kwon@gmail.com>
2023-05-31 11:07:57 +0900

    프로그래머스: KAKAO [3차]파일명 정렬 (통과 / head,num,tail 분류 수정)

--- src/etc/kakao/SortByFileName.java
+++ src/etc/kakao/SortByFileName.java
@@ -7,43 +7,30 @@ public class SortByFileName {
 
   public String[] solution(String[] files){
     String[] answer = new String[files.length];
-    String[] filesHEAD = new String[files.length];
-    String[] filesNUMBER = new String[files.length];
-    String[] filesTAIL = new String[files.length];
     String[][] answer2 = new String[files.length][3];
 
     for (int i = 0; i < files.length; i++) {
-      int j, k=0, l=0;
+      int j, k=0, startIdx=0, size = 0;
       boolean state = false;
+
       for (j = 0; j < files[i].length(); j++) {
-
-        if(files[i].charAt(j) >= 48 && files[i].charAt(j) <= 57){
-          if(!state && files[i].charAt(j) > 48 && files[i].charAt(j) <= 57){
-            l = j;
+        if(Character.isDigit(files[i].charAt(j))){
+          if(!state){
+            startIdx = j;
             state = true;
-            continue;
           }
-          if(!state && files[i].charAt(j) == 48){
-            k++;
-            continue;
+          size++;
+        } else {
+          if (state){
+            break;
           }
-
-        }
-        if(state && !(files[i].charAt(j) >= 48 && files[i].charAt(j) <= 57)){
-          break;
         }
       }
-      filesHEAD[i] = files[i].substring(0,l-k);
-      filesNUMBER[i] = files[i].substring(l-k,j);
-      filesTAIL[i] = files[i].substring(j);
-      answer2[i][0] = files[i].substring(0,l-k);
-      answer2[i][1] = files[i].substring(l-k,j);
-      answer2[i][2] = files[i].substring(j);
+      int endIdx = startIdx + size - 1;
+      answer2[i][0] = files[i].substring(0,startIdx);
+      answer2[i][1] = files[i].substring(startIdx, endIdx + 1);
+      answer2[i][2] = files[i].substring(endIdx + 1);
     }
-
-    System.out.println(Arrays.toString(filesHEAD));
-    System.out.println(Arrays.toString(filesNUMBER));
-    System.out.println(Arrays.toString(filesTAIL));
 
     Arrays.sort(answer2, (o1, o2) ->{
       if(o1[0].toLowerCase().compareTo(o2[0].toLowerCase(Locale.ROOT)) > 0){
