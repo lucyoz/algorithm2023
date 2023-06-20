d800de1cda15044003bca9e714e877b6a6d06868
lucyoz <lucyoz.kwon@gmail.com>
2023-06-20 11:02:18 +0900

    프로그래머스: 숫자 목록 (통과 / 범위 주의)

--- src/etc/practice/NumBlock.java
+++ src/etc/practice/NumBlock.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class NumBlock {
   //소수인 경우 1이 나오고
   //그 외에는 약수의 (본인을 제외한) 최댓값
+  //end의 최댓값과(>) 숫자블록의 최댓값이 다름! 주의
 
   public int[] solution(long begin, long end){
     List<Integer> list = new ArrayList<>();
@@ -15,11 +16,8 @@ public class NumBlock {
         list.add(0);
         continue;
       }
-      if(isPrime(i)){
-        list.add(1);
-      } else {
-        list.add(findSecondMax(i));
-      }
+      list.add(findSecondMax(i));
+
     }
 
     return list.stream().mapToInt(Integer::intValue).toArray();
@@ -37,13 +35,20 @@ public class NumBlock {
 
   //약수 중 본인을 제외한 최대값 찾기
   private int findSecondMax(long num){
+    List<Integer> list = new ArrayList<>();
     int i;
     for (i = 2; i <= Math.sqrt(num); i++) {
       if(num%i==0){
-        break;
+        list.add(i);
+        if(num/i <= 10_000_000){
+          return (int)num/i;
+        }
       }
     }
-    return (int)num/i;
+    if(list.size()>0){
+      return list.get(list.size()-1);
+    }
+    return 1;
   }
 
   public static void main(String[] args) {
